2377d0bcac21241041fea3e79d539295bd06dbd9
goldenkm <894082241@qq.com>
2026-02-28 12:27:16 +0800

    fix: finish sync commit-d7f48f7 of osworld

--- desktop_env/osworld/controllers/python.py
+++ desktop_env/osworld/controllers/python.py
@@ -476,6 +476,12 @@ class PythonController:
         """
         return self.execute_python_command("import platform; print(platform.system())")['output'].strip()
 
+    def get_vm_machine(self):
+        """
+        Gets the machine of the vm.
+        """
+        return self.execute_python_command("import platform; print(platform.machine())")['output'].strip()
+
     def get_vm_screen_size(self):
         """
         Gets the size of the vm screen.
